674bb8ef507fa6dcb0d11bcf61d1ce31912be6eb
philipphomberger <philipp.homberger@gmail.com>
2024-05-16 16:09:05 +0200

    add somm code dokumentation

--- server/server.go
+++ server/server.go
@@ -20,7 +20,11 @@ func Server() {
 		fmt.Println(err)
 		return
 	}
-	defer ln.Close()
+	defer func(ln *net.UDPConn) {
+		err := ln.Close()
+		if err != nil {
+		}
+	}(ln)
 	for {
 		tmp := make([]byte, 1024)
 		_, addr, _ := ln.ReadFromUDP(tmp)
@@ -42,21 +46,29 @@ func serveDNS(u *net.UDPConn, clientAddr net.Addr, request *layers.DNS) {
 	// Get IP Adress from JSON Database.
 	ip = dns.GetIPAdress(string(request.Questions[0].Name), database)
 	a, _, _ := net.ParseCIDR(ip + "/24")
+	// SetUP Dns Answer
 	dnsAnswer.Type = layers.DNSTypeA
 	dnsAnswer.IP = a
 	dnsAnswer.Name = request.Questions[0].Name
 	dnsAnswer.Class = layers.DNSClassIN
+	// Setup ReplyMess Anwer
 	replyMess.QR = true
 	replyMess.ANCount = 1
 	replyMess.OpCode = layers.DNSOpCodeNotify
 	replyMess.AA = true
 	replyMess.Answers = append(replyMess.Answers, dnsAnswer)
 	replyMess.ResponseCode = layers.DNSResponseCodeNoErr
+	// Convert to binary
 	buf := gopacket.NewSerializeBuffer()
 	opts := gopacket.SerializeOptions{} // See SerializeOptions for more details.
 	err = replyMess.SerializeTo(buf, opts)
 	if err != nil {
 		panic(err)
 	}
-	u.WriteTo(buf.Bytes(), clientAddr)
+	// Send Answer to DNS Client
+	to, err := u.WriteTo(buf.Bytes(), clientAddr)
+	if err != nil {
+		return
+	}
+	_ = to
 }
